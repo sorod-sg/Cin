e5ac55a7a63f89bf71b48489cc98d06c6fb7b965
sorod_sg <guo_fu_yu@qq.com>
2021-09-08 21:46:00 +0800

    add routergroup

--- gee.go
+++ gee.go
@@ -1,29 +1,45 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*context) //归类handler
 
 type Engine struct {
+	*RouterGroup
 	router *router
-} //使用engine储存路由表
+	group  []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix     string
+	middewares []HandlerFunc
+	parent     *RouterGroup
+	engine     *Engine //将框架中所有的资源统一交给engine进行管理
+}
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.group = []*RouterGroup{engine.RouterGroup}
+	return engine
+
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 } //添加路由
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
@@ -33,4 +49,15 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.group = append(engine.group, newGroup)
+	return newGroup
 }
